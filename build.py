fa1b68e4179a82a3ee22751621f2b0c2c81ca838
Mukhtar Kiboigo <mukhtarkiboigo@gmail.com>
2021-09-25 21:33:27 -0700

    Changed build.py to utilize subprocess.

--- build.py
+++ build.py
@@ -1,14 +1,14 @@
-import os
-
 # Installs the requirements needed to build.
-os.system("pip3 install -r requirements.txt")
+import subprocess
+
+subprocess.run(['pip3', 'install', '-r', 'requirements.txt'], shell=False)
 
 # Sorts all the src files.
-os.system("isort src/*")
+subprocess.run(['isort', 'src/*'])
 print("All source files have been sorted successfully.")
 
 # Stylechecks all the src files.
-os.system("black src/*")
+subprocess.run(['black', 'src/*'])
 
 # Compiles Anvil into an .exe for Windows.
-os.system("pyinstaller --onefile --clean --name Anvil src/main.py")
+subprocess.run(['pyinstaller', '--onefile', '--clean', '--name', 'Anvil', 'src/main.py'])
