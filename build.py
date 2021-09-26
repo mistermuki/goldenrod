454105411fbeaa7e481fc7d04f87a46ff41d7ca9
Mukhtar Kiboigo <mukhtarkiboigo@gmail.com>
2021-09-26 00:47:33 -0700

    Updated Build.py again.

--- build.py
+++ build.py
@@ -1,17 +1,27 @@
 import platform
 import subprocess
 
-import isort
-
 subprocess.run(["pip3", "install", "-r", "requirements.txt"], shell=False)
 
 # Sorts all the src files.
-isort.code("src/*")
+try:
+    import isort
+
+    isort.code("src/*")
+except:
+    if platform.system() == "Linux":
+        subprocess.run(["python3", "-m", "isort", "src/"])
+    else:
+        subprocess.run(["python", "-m", "isort", "src/"])
+
+
 print("All source files have been sorted successfully.")
-
 # Stylechecks all the src files.
-subprocess.run(["python3", "-m", "black", "src/"])
 
+if platform.system() == "Linux":
+    subprocess.run(["python3", "-m", "black", "src/"])
+else:
+    subprocess.run(["python", "-m", "black", "src/"])
 
 if platform.system() == "Linux":
     print("Will not compile the exe because we are on Linux.")
