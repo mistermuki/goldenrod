48b967c95424b8994db9e5a6acc4c5256f3050f4
Muki Kiboigo <mukhtarkiboigo@gmail.com>
2021-09-28 20:12:37 -0700

    0.3.0 Version Bump

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ import sys
 
 from cx_Freeze import Executable, setup
 
-# base="Win32GUI" should be used only for Windows GUI app
-base = "Console"
-
-target = Executable(script="src/anvil.py", base=base, icon="src/icon.ico")
+target = Executable(script="src/anvil.py", base= None, icon="src/icon.ico")
+packages = ["multiprocessing"]
 
 setup(
     name="Anvil",
-    version="0.2.0",
+    version="0.3.0",
     description="Anvil is a lightweight CLI Forge Profile Manager.",
     executables=[target],
-    options={"build.exe": {}},
+    options={"build.exe": {"packages":packages}}
 )
